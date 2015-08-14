4c556d988e8ce234ed88e48b1328541d96793145
cdg77 <cdegregorio@gmail.com>
2015-08-14 10:58:10 -0700

    It save a new word to the array. 4 passing tests.

--- lib/word.rb
+++ lib/word.rb
@@ -1,6 +1,8 @@
 class Word
+  @@words = []
 
-  define_method(:initialize) do | type, syllable |
+  define_method(:initialize) do | word, type, syllable |
+    @word = word
     @type = type
     @syllable = syllable
     @definition = []
@@ -13,5 +15,16 @@ class Word
   define_method(:syllable) do
     @syllable
   end
+
+  define_method(:word) do
+    @word
+  end
+
+
+  define_method(:save) do
+    @@words.push(self)
+  end
+
+  # define_singleton_method()
 
 end
--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -5,15 +5,29 @@ describe(Word) do
 
   describe('#type') do
     it('returns the type of word') do
-      test_word = Word.new('noun', 'def i ni tion')
+      test_word = Word.new('definition', 'noun', 'def i ni tion')
       expect(test_word.type).to(eq('noun'))
     end
   end
 
   describe('#syllable') do
     it('returns the word seperated at each syllable') do
-      test_word = Word.new('noun', 'def i ni tion')
+      test_word = Word.new('definition', 'noun', 'def i ni tion')
       expect(test_word.syllable).to(eq('def i ni tion'))
+    end
+  end
+
+  describe('#word') do
+    it('returns the word') do
+      test_word = Word.new('definition', 'noun', 'def i ni tion')
+      expect(test_word.word).to(eq('definition'))
+    end
+  end
+
+  describe('#save') do
+    it('save a new word') do
+      test_word = Word.new('definition', 'noun', 'def i ni tion')
+      expect(test_word.save).to(eq([test_word]))
     end
   end
 
