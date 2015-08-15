bc8f94e647322a1cffa1977dc458b91a3319d5f6
cdg77 <cdegregorio@gmail.com>
2015-08-14 17:29:40 -0700

    added some front end stuff

--- app.rb
+++ app.rb
@@ -1,6 +1,47 @@
-# require ('sinatra')
-# require ('sinatra/reloader')
+require ('sinatra')
+require ('sinatra/reloader')
+also_reload('lib/**/*.rb')
+require('./lib/word')
+require('./lib/definition')
 
-# get('/') do
-#   erb(:index)
+
+get('/') do
+  @word = Word.all()
+  erb(:index)
+end
+
+get('/word') do
+  @word = Word.all()
+  erb(:word)
+end
+
+# post('/lemons') do
+#   make = params.fetch('make')
+#   model = params.fetch('model')
+#   year = params.fetch('year')
+#   vehicle = Vehicle.new(make, model, year)
+#   vehicle.save()
+#   erb(:success)
 # end
+
+
+post('/word') do
+  definition  = params.fetch('definition')
+  id_number   = params.fetch('id')
+  @definition = Definition.new(:definition => definition)
+  @definition.save()
+  @word = Word.find(id_number.to_i)
+  @word.add_definition(@definition)
+  erb(:word)
+end
+
+get('/word/:id/') do
+  @word = Word.find(params.fetch('id').to_i)
+  erb(:word)
+end
+
+
+get('/word/new') do
+  @words = Word.all()
+  erb(:word_form)
+end
--- spec/word_integration_spec.rb
+++ spec/word_integration_spec.rb
@@ -1,4 +1,10 @@
-# require('capybara/rspec')
-# require('./app')
-# Capybara.app = Sinatra::Application
-# set(:show_exceptions, false)
+require('capybara/rspec')
+require('./app')
+Capybara.app = Sinatra::Application
+set(:show_exceptions, false)
+
+describe('the path of word', {:type => :feature}) do
+  it('shows the index page')  do
+    visit('/')
+    expect(page).to have_content("Words Defined")
+  end
